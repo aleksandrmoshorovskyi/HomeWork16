2f9072536900fa4698dcfb463b625473b4a0ee52
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2024-04-17 16:15:32 +0300

    init

---
+++ HomeWork16/OnboardingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewController.swift
+//  HomeWork16
+//
+//  Created by Aleksandr Moroshovskyi on 17.04.2024.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+}
---
+++ HomeWork16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork16
+//
+//  Created by Aleksandr Moroshovskyi on 17.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
